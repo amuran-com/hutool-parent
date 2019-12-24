82619827286329c4309068dfa7c28419e979587c
amuran-com <dream@amuran.com>
2019-12-24 22:23:41 +0800

    Initial commit

---
+++ hutool-http/src/main/java/cn/hutool/http/Test.java
@@ -0,0 +1,15 @@
+package cn.hutool.http;
+
+/**
+ * @author DREAM
+ * @title: Test
+ * @projectName hutool-parent
+ * @description: TODO
+ * @date 2019-12-1714:43
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
